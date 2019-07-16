c0c9be33a82e1fe757ff6f7077d9b49bff54548d
shimingyah <shimingyah@gmail.com>
2019-07-16 18:06:53 +0800

    add conn struct once field

--- conn.go
+++ conn.go
@@ -24,6 +24,7 @@ type Conn interface {
 	Value() *grpc.ClientConn
 
 	// Close decrease the reference of grpc connection, instead of close it.
+	// if the pool is full, just close it.
 	Close() error
 }
 
@@ -31,6 +32,7 @@ type Conn interface {
 type conn struct {
 	cc   *grpc.ClientConn
 	pool *pool
+	once bool
 }
 
 // Value see Conn interface.
@@ -41,12 +43,27 @@ func (c *conn) Value() *grpc.ClientConn {
 // Close see Conn interface.
 func (c *conn) Close() error {
 	c.pool.decrRef()
+	if c.once {
+		return c.reset()
+	}
 	return nil
 }
 
-func (p *pool) wrapConn(cc *grpc.ClientConn) *conn {
+func (c *conn) reset() error {
+	cc := c.cc
+	c.cc = nil
+	c.pool = nil
+	c.once = false
+	if cc != nil {
+		return cc.Close()
+	}
+	return nil
+}
+
+func (p *pool) wrapConn(cc *grpc.ClientConn, once bool) *conn {
 	return &conn{
 		cc:   cc,
 		pool: p,
+		once: once,
 	}
 }
