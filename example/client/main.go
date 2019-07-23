e6c98e362a1c795a82cb8c6ef277111a359e54a1
shimingyah <shimingyah@gmail.com>
2019-07-23 19:50:42 +0800

    add simple usage

---
+++ example/client/main.go
@@ -0,0 +1,54 @@
+// Copyright 2019 shimingyah. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// ee the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/shimingyah/pool"
+	"github.com/shimingyah/pool/example/pb"
+)
+
+var addr = flag.String("addr", "localhost:50000", "the address to connect to")
+
+func main() {
+	flag.Parse()
+
+	p, err := pool.New(*addr, pool.DefaultOptions)
+	if err != nil {
+		log.Fatalf("failed to new pool: %v", err)
+	}
+	defer p.Close()
+
+	conn, err := p.Get()
+	if err != nil {
+		log.Fatalf("failed to get conn: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewEchoClient(conn.Value())
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	res, err := client.Say(ctx, &pb.EchoRequest{Message: []byte("hi")})
+	if err != nil {
+		log.Fatalf("unexpected error from Say: %v", err)
+	}
+	fmt.Println("rpc response:", res)
+}
---
+++ example/server/main.go
@@ -0,0 +1,66 @@
+// Copyright 2019 shimingyah. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// ee the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
+
+	"github.com/shimingyah/pool"
+	"github.com/shimingyah/pool/example/pb"
+)
+
+var port = flag.Int("port", 50000, "port number")
+
+// server implements EchoServer.
+type server struct{}
+
+func (s *server) Say(context.Context, *pb.EchoRequest) (*pb.EchoResponse, error) {
+	return &pb.EchoResponse{Message: []byte("hello world")}, nil
+}
+
+func main() {
+	flag.Parse()
+
+	listen, err := net.Listen("tcp", fmt.Sprintf(":%v", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer(
+		grpc.InitialWindowSize(pool.InitialWindowSize),
+		grpc.InitialConnWindowSize(pool.InitialConnWindowSize),
+		grpc.MaxSendMsgSize(pool.MaxSendMsgSize),
+		grpc.MaxRecvMsgSize(pool.MaxRecvMsgSize),
+		grpc.KeepaliveEnforcementPolicy(keepalive.EnforcementPolicy{
+			PermitWithoutStream: true,
+		}),
+		grpc.KeepaliveParams(keepalive.ServerParameters{
+			Time:    pool.KeepAliveTime,
+			Timeout: pool.KeepAliveTimeout,
+		}),
+	)
+	pb.RegisterEchoServer(s, &server{})
+
+	if err := s.Serve(listen); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
